5a0c2d2395f5277125c4f5e1225b11d64348eee0
Gou Furuya <innocent.zero@gmail.com>
2016-06-05 16:52:22 +0900

    Add callback method

--- lib/lita/handlers/amazon_jp_search.rb
+++ lib/lita/handlers/amazon_jp_search.rb
@@ -6,7 +6,31 @@ module Lita
       # insert handler code here
       route(/^amazon\s(.+)/i, :search_amazon)
 
+      def search_amazon(response)
+        client = Mechanize::AmazonJpSearch::Client.new(3)
+        query = response.matches.first.join(' ')
+
+        search_result = client.search(query)
+
+        search_result.each do |result|
+          price = separated_digit(result[:price])
+          response.reply "#{result[:title]} ￥#{price}\n#{result[:href]}"
+        end
+      end
+
       Lita.register_handler(self)
+
+      private
+
+      def separated_digit(number)
+        number.to_s
+              .chars
+              .reverse
+              .map
+              .with_index(1) { |x, i| i.modulo(3).zero? ? ",#{x}" : x  }
+              .reverse
+              .join('')
+      end
     end
   end
 end
