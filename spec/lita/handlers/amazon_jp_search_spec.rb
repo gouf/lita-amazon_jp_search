40168ac142803bacba2f651b1afc576d7c44766d
Gou Furuya <innocent.zero@gmail.com>
2016-06-05 16:47:56 +0900

    Add spec

--- spec/lita/handlers/amazon_jp_search_spec.rb
+++ spec/lita/handlers/amazon_jp_search_spec.rb
@@ -1,4 +1,12 @@
 require "spec_helper"
 
 describe Lita::Handlers::AmazonJpSearch, lita_handler: true do
+  it { is_expected.to route('amazon ruby') }
+  it { is_expected.to_not route('amazon') }
+  it { is_expected.to route('amazon ruby').to(:search_amazon) }
+
+  it 'reply 3 list' do
+    send_command('amazon ruby')
+    expect(replies.size).to eq 3
+  end
 end
